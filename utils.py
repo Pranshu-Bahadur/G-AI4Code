f94e580e25c7db59582a7ad75410703ae4404214
Pranshu Bahadur <pranshubahadur@gmail.com>
2022-08-01 02:19:22 +0530

    begin

--- utils.py
+++ utils.py
@@ -9,17 +9,29 @@ exec(open(''.join(rootdir2Df('libs').applymap(str)['Path'].values), 'r').read())
 
 class Pathfinder(object):
     def __init__(self, rootdir : str, fextstr : str = "**/*.*"):
-        df = rootdir2Df(rootdir)
-        df = DataFrame(merge(df, df['Path'].apply(str).str.extract(r'.([a-z]{3,4}$)'), \
-                right_index=True, left_index=True))
+        self.df = rootdir2Df(rootdir)
+        self.fext = lambda fextstr: self.df[self.df['Path'].apply(str).str.contains(fextstr)]
 
-        self.meta = {
-                'fpaths_df' : df
-                }
 class ADataset(Dataset, Pathfinder):
     def __init__(self, rootdir : str, fextstr : str = "**/*.*"):
         super().__init__(rootdir)
-        print(self.meta)
+
+        self.jsons = self.fext('json')
+        self.csvs = self.fext('csv')
+
+        # get dist?
+
+
+
+
+        
+        #test = eval(open(DataFrame(self.meta['fpaths_df'].groupby(0)).to_dict()[1][1].iloc[0]['Path'], 'r').read())
+
+        #test = DataFrame(test)
+
+        #print(test)
+
+
 
 
 
